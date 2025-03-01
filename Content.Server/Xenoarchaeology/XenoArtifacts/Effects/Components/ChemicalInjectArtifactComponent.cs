54fe6ac90eafa90344d5fe1954f7dbdc2a8861ef
shibe <swiftshibe@gmail.com>
2025-03-01 04:29:32 -0500

    wwwwww

---
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Components/ChemicalInjectArtifactComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
+
+/// <summary>
+/// When activated, will shuffle the position of all players
+/// within a certain radius.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ChemicalInjectArtifactComponent : Component
+{
+    [DataField("radius")]
+    public float Radius = 6f;
+
+    /// <summary>
+    ///     If true, the artifact will only inject whatever entity activated it
+    /// </summary>
+    [DataField("onlyEffectsActivator")]
+    public bool onlyEffectsActivator = false;
+}
---
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/ChemicalInjectArtifactSystem.cs
@@ -0,0 +1,25 @@
+﻿using Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
+using Content.Server.Xenoarchaeology.XenoArtifacts.Events;
+using Content.Shared.Mobs.Components;
+using Robust.Shared.Map;
+using Robust.Shared.Random;
+
+namespace Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Systems;
+
+public sealed class ChemicalInjectArtifactSystem : EntitySystem
+{
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly SharedTransformSystem _xform = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ChemicalInjectArtifactComponent, ArtifactActivatedEvent>(OnActivated);
+    }
+
+    private void OnActivated(EntityUid uid, ChemicalInjectArtifactComponent component, ArtifactActivatedEvent args)
+    {
+
+    }
+}
